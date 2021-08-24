1e21ffaa5b0e646b12023fb25c56683090dc7052
ElzatAhmed <1208915986@qq.com>
2021-08-24 23:17:13 +0800

    project created

---
+++ src/main/java/team/peiYangCoders/PeiYangResourceManagement/PeiYangResourceManagementApplication.java
@@ -0,0 +1,13 @@
+package team.peiYangCoders.PeiYangResourceManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeiYangResourceManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeiYangResourceManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/team/peiYangCoders/PeiYangResourceManagement/PeiYangResourceManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package team.peiYangCoders.PeiYangResourceManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeiYangResourceManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
